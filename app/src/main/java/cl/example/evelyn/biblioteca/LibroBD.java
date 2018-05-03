9ce3ff0d43c4ecd239866664b79e36e14cbc6c73
Panchosama <panchosama@gmail.com>
2018-05-03 10:48:34 -0300

    Varios cambios [EM] Se crean las opciones de registrar préstamo y a quién [EM] Se modifica base de datos para registrar préstamos [FF] Se corrige script para base de datos [FF] Se corrige activity de listado, con colores oficiales [FF] Se modifica el Manifest, para prevenir el inicio automático del teclado

--- app/src/main/java/cl/example/evelyn/biblioteca/LibroBD.java
+++ app/src/main/java/cl/example/evelyn/biblioteca/LibroBD.java
@@ -11,7 +11,14 @@ import java.util.ArrayList;
  */
 
 public class LibroBD extends SQLiteOpenHelper {
-    private String consultaCreate ="CREATE TABLE libro(id_libro INTEGER PRIMARY KEY AUTOINCREMENT , titulo TEXT NOT NULL,autor   TEXT NOT NULL,editor TEXT NOT NULL,fecha TEXT NOT NULL,descripcion TEXT NOT NULL,cantidad TEXT NOT NULL,estado boolean NOT NULL default 0,descripcionestado TEXT NULL) ";
+    private String consultaCreate ="CREATE TABLE libro(" +
+            "id_libro INTEGER PRIMARY KEY AUTOINCREMENT , " +
+            "titulo TEXT NOT NULL,autor   TEXT NOT NULL," +
+            "editor TEXT NOT NULL,fecha TEXT NOT NULL," +
+            "descripcion TEXT NOT NULL," +
+            "cantidad TEXT NOT NULL," +
+            "estado boolean NOT NULL default 0, " +
+            "descripcionestado TEXT)";
     public LibroBD(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -38,7 +45,7 @@ public class LibroBD extends SQLiteOpenHelper {
             }while(lista.moveToNext());
 
         }
-        return  listar;
+        return listar;
     }
     public ArrayList listarPorTitulo(String titulo){
         ArrayList<Libro> listar = new ArrayList<>();
